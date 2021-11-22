eed1ba0d61a9453ad702eef4fd0b0c97dcd2d043
Eduardo Fabre <egufabre@hotmail.com>
2021-11-22 16:12:18 -0300

    se agregan las rutas de productos

--- app-productos/routes/api.php
+++ app-productos/routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/producto', [ProductoController::class,"Listar"]);
+Route::get('/producto/{d}', [ProductoController::class,"ListarUno"]);
+Route::post('/producto', [ProductoController::class,"Agregar"]);
+Route::put('/producto', [ProductoController::class,"Modificar"]);
+Route::delete('/producto', [ProductoController::class,"Eliminar"]);
