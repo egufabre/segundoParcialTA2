264a880da9208f4b9ff2fbe78f87052ba44323b7
SantiagoBonilla1 <santiagobonillaeguren@gmail.com>
2021-11-23 14:06:46 -0300

    Agregadas más routas

--- app-clientes/routes/web.php
+++ app-clientes/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'App\Http\Controllers\PostController@Inicio');
+Route::get('/newPost',function () {
+    return view('crearPost');
 });
+
+Route::get('/crearUsuario', function () {
+    return view('crearUsuario');
+});
+Route::post('/crearUsuario','App\Http\Controllers\ClienteController@createUser');
+
+Route::get("/perfil/{d}",'App\Http\Controllers\ClienteController@showProfile');
+Route::get('/editPost','App\Http\Controllers\PostController@editPostForm');
+Route::post('/editPost','App\Http\Controllers\PostController@editPost');
+
+Route::post('/newPost','App\Http\Controllers\PostController@newPost');
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::post('/login','App\Http\Controllers\ClienteController@autenticar');
