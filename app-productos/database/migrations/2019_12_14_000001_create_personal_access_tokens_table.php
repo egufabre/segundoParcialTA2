ef71b9fb9df5f4cf17b99b00feec4021ca243811
Eduardo Fabre <egufabre@hotmail.com>
2021-11-23 15:18:38 -0300

    LIMPIANDO

--- app-productos/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ app-productos/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,7 +13,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
             $table->string("nombre");
@@ -22,16 +21,6 @@ class CreatePersonalAccessTokensTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-=======
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
->>>>>>> cliente
         });
     }
 
@@ -42,12 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-<<<<<<< HEAD
         Schema::dropIfExists('productos');
     }
 }
-=======
-        Schema::dropIfExists('personal_access_tokens');
-    }
-}
->>>>>>> cliente
--- app-productos/routes/api.php
+++ app-productos/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\ProductoController;
 
 
@@ -11,20 +10,3 @@ Route::get('/producto/{d}', [ProductoController::class,"ListarUno"]);
 Route::post('/producto', [ProductoController::class,"Agregar"]);
 Route::put('/producto', [ProductoController::class,"Modificar"]);
 Route::delete('/producto', [ProductoController::class,"Eliminar"]);
-=======
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
->>>>>>> cliente
