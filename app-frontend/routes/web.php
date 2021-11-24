b70fffae7b0b2d036215d6327a06d683bf9b8cae
SantiagoBonilla1 <santiagobonillaeguren@gmail.com>
2021-11-24 17:27:25 -0300

    Agregada las rutas nuevamente

---
+++ app-frontend/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProductoController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UsuarioController;
+
+
+Route::get('/Listar', [UsuarioController::class,"ListarUsuarios"]);
+Route::get('/Listar/{d}', [UsuarioController::class,"ListarUsuario"]);
+
+Route::get('/agregarCliente', function () {
+    return view('formAgregarUsuario');
+});
+Route::post('/agregarCliente', [UsuarioController::class,"AgregarUsuario"]);
+
+Route::get('/agregarVendedor', function () {
+    return view('formAgregarUsuario');
+});
+Route::post('/agregarVendedor', [UsuarioController::class,"AgregarVendedor"]);
+
+Route::get('/modificarUsuario', function () {
+    return view('formModificarUsuario');
+});
+Route::post('/modificarUsuario', [UsuarioController::class,"ModificarUsuario"]);
+
+Route::get('/eliminarUsuario', function () {
+    return view('formEliminarUsuario');
+});
+Route::post('/eliminarUsuario', [UsuarioController::class,"EliminarUsuario"]);
+
+route::get('/listarProductos', [ProductoController::class,"ListarProductos"]);
+Route::get('/agregarProductos', function () {
+    return view('formAgregarProducto');
+});
+
+Route::post('/agregarProductos', [ProductoController::class,"AgregarProducto"]);
+
+Route::get('/eliminarProductos', function () {
+    return view('formEliminarProducto');
+});
+Route::post('/eliminarProductos', [UsuarioController::class,"EliminarProductos"]);
