f93fb77046e00b3e188e0f5006294e6359869830
Eduardo Fabre <egufabre@hotmail.com>
2021-11-24 15:18:01 -0300

    controlador producto en frontend creado

---
+++ app-frontend/app/Http/Controllers/ProductoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Producto;
+
+
+
+class ProductoController extends Controller
+{
+    public function ListarProductos(Request $request){
+        $producto = Http::get(getenv("APP_PRODUCTOS_URL") . "producto") -> json();
+        return view('listaProductos',["producto" => $producto]);
+    }
+
+    public function AgregarProducto(Request $request){
+        $response = Http::post(getenv("APP_PRODUCTOS_URL") . "producto", [
+            'nombre' => $request -> post('nombre'),
+            'descripcion' => $request -> post('descripcion'),
+            'stock' => $request -> post('stock'),
+        ]) -> json();
+        if($response["resultado"] === "OK")
+        return view('formAgregarProducto',["exito" => true]);
+        else {
+            return "ERROR";
+        }
+
+    }
+}
